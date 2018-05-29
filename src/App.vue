ac2c8cfca944aca3122df0c1ac421151724b239c
nitinagarwal-selotsoft <nitina@selotsoft.com>
2018-05-29 15:11:01 +0530

    basic view structure created

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,24 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
+  <div>
+      <transition name="page" mode="out-in">
+        <router-view></router-view>
+      </transition>
   </div>
 </template>
 
 <script>
+import 'vuetify/dist/vuetify.min.css'
 export default {
   name: 'app'
 }
 </script>
 
 <style>
-body {
-  margin: 0;
+.page-enter-active, .page-leave-active {
+  transition: opacity 0.7s, transform 0.7s;
 }
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
+.page-enter, .page-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Login from '@/components/Login'
+import Setting from '@/components/Setting'
+import TableList from '@/components/TableList'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/setting',
+      name: 'Setting',
+      component: Setting
+    },
+    {
+      path: '/table',
+      name: 'TableList',
+      component: TableList
     }
   ]
 })
